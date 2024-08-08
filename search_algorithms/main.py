59dae5e552acdc0880a1ede3b6b84a4c45a636af
gventino <g.ventinos@gmail.com>
2024-08-08 20:45:43 -0300

    wrong origin url... oops

--- search_algorithms/main.py
+++ search_algorithms/main.py
@@ -1,7 +1,13 @@
 from utils.HrefExtractor import HrefExtractor
-
-test = HrefExtractor('https://ufu.com.br')
+import networkx as nx
+import matplotlib.pyplot as plt
+test = HrefExtractor('https://ufu.br')
 print('ORIGIN:\t' + test.url)
 print('HREFs:')
 for href in test.hrefs:
     print('\t' + href)
+
+G = nx.Graph()
+G.add_node(test)
+nx.draw(G)
+plt.show()
